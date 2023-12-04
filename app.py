9b4ed5e0109863ba5ae42e7d82e97dec8390c946
limxuankai <limxuankai3645@gmail.com>
2023-12-04 21:07:32 +0800

    Logging

--- app.py
+++ app.py
@@ -15,11 +15,22 @@ from oauthlib.oauth2 import WebApplicationClient
 import requests
 from db import init_db_command
 from user import User
+import logging
+from logging.handlers import RotatingFileHandler
+
 app = Flask(__name__)
 
+app.logger.setLevel(logging.INFO)
+for handler in app.logger.handlers[:]:
+    app.logger.removeHandler(handler)
+file_handler = RotatingFileHandler('app.log', mode='a', maxBytes=0, backupCount=0)
+formatter = logging.Formatter('%(asctime)s: %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+app.logger.addHandler(file_handler)
+
+app.logger.propagate = False
+
 app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
-app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(hours=1)
-app.config['SESSION_COOKIE_SECURE'] = True
 GOOGLE_CLIENT_ID = "326380353762-pek05h1clqj4likpcjtqba5gdf831okv.apps.googleusercontent.com"
 GOOGLE_CLIENT_SECRET = "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"
 GOOGLE_DISCOVERY_URL = (
@@ -32,7 +43,7 @@ login_manager.init_app(app)
 try:
     init_db_command()
 except sqlite3.OperationalError:
-    pass
+    app.logger.error("Error:sqlite3.OperationalError")
 
 client = WebApplicationClient(GOOGLE_CLIENT_ID)
 
@@ -43,9 +54,7 @@ def load_user(user_id):
 @app.route('/',methods=['GET','POST'])
 @app.route('/home',methods=['GET','POST'])
 def home():
-    if current_user.is_authenticated:
-        return redirect(url_for("dashboard"))
-    else:
+        app.logger.info('Guest has reached homepage')
         return render_template('home.html')
 
 def get_google_provider_cfg():
@@ -53,6 +62,7 @@ def get_google_provider_cfg():
 
 @app.route("/login")
 def login():
+    app.logger.info('Guest is trying to login')
     google_provider_cfg = get_google_provider_cfg()
     authorization_endpoint = google_provider_cfg["authorization_endpoint"]
 
@@ -65,6 +75,7 @@ def login():
 
 @app.route('/login/callback', methods=['GET','POST'])
 def callback():
+    app.logger.info('Guest is authenticating on Google')
     code = request.args.get("code")
     google_provider_cfg = get_google_provider_cfg()
     token_endpoint = google_provider_cfg["token_endpoint"]
@@ -97,30 +108,27 @@ def callback():
     user = User(
     id_=unique_id, name=users_name, email=users_email, profile_pic=picture
 )
-
-    # Doesn't exist? Add it to the database.
     if not User.get(unique_id):
         User.create(unique_id, users_name, users_email, picture)
-
-    # Begin user session by logging the user in
     login_user(user)
-
-    # Send user back to homepage
     return redirect(url_for("dashboard"))
 
 @app.route('/dashboard', methods=['GET','POST'])
 @login_required
 def dashboard():
+    app.logger.info(f'{current_user.name} has logged in')
     return render_template("dashboard.html")
 
 @app.route("/logout")
 @login_required
 def logout():
+    app.logger.info(f'{current_user.name} has logged out')
     logout_user()
     return redirect(url_for("home"))
 
 @app.route('/about')
 def aboutus():
+    app.logger.info("Guest has arrived about us")
     return render_template("aboutus.html")
 
 if __name__ == '__main__':
